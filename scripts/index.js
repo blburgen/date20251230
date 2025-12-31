1d16c4d85cb9e2aa0347baef87dea31941316917
Brady Burgener <bradylbsee@gmail.com>
2025-12-31 12:49:50 -0700

    added question

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,54 @@
 let opening = document.getElementById("opening");
 let open = document.getElementById("open");
 let part1 = document.getElementById("part1");
+let part1a = document.getElementById("part1a");
+let part1b = document.getElementById("part1b");
+let part1c = document.getElementById("part1c");
+let flour = document.getElementById("flour");
+let gobacktopart1 = document.getElementById("gobacktopart1");
 let gotopart2 = document.getElementById("gotopart2");
+let gobacktopart2 = document.getElementById("gobacktopart2");
 let part2 = document.getElementById("part2");
 let gotopart3 = document.getElementById("gotopart3");
+let gobacktopart3 = document.getElementById("gobacktopart3");
 let part3 = document.getElementById("part3");
 let gotopart4 = document.getElementById("gotopart4");
+let recipe = document.getElementById("recipe");
+let wrong = document.getElementById("wrong");
 
 opening.addEventListener("click",()=>{
     open.classList.add("hidden");
     part1.classList.remove("hidden");
+    part1a.classList.remove("hidden");
+    recipe.classList.remove("hidden");
+    wrong.classList.add("hidden");
 });
+
+part1c.addEventListener("click",()=>{
+    if(part1b.value.toLowerCase() == "flour"){
+        flour.classList.remove("hidden");
+        part1a.classList.add("answered")
+        wrong.classList.add("hidden");
+    }else{
+        wrong.classList.remove("hidden");
+    }
+})
 
 gotopart2.addEventListener("click",()=>{
     part1.classList.add("hidden");
+    part1a.classList.add("hidden");
+    part2.classList.remove("hidden");
+    wrong.classList.add("hidden");
+});
+
+gobacktopart1.addEventListener("click",()=>{
+    part2.classList.add("hidden");
+    part1.classList.remove("hidden");
+    part1a.classList.remove("hidden");
+});
+
+gobacktopart2.addEventListener("click",()=>{
+    part3.classList.add("hidden");
     part2.classList.remove("hidden");
 });
 
@@ -27,7 +62,13 @@ gotopart4.addEventListener("click",()=>{
     part4.classList.remove("hidden");
 });
 
+gobacktopart3.addEventListener("click",()=>{
+    part4.classList.add("hidden");
+    part3.classList.remove("hidden");
+});
+
 gotopart5.addEventListener("click",()=>{
     part4.classList.add("hidden");
     part5.classList.remove("hidden");
+    recipe.classList.add("hidden");
 });
